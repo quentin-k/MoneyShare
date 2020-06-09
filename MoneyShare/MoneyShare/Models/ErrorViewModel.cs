e1d2471c3c2dfabea78ccfea7cd45495d66a9993
Quentin <49767738+quentin-k@users.noreply.github.com>
2020-06-09 16:47:01 -0700

    Initialize repo

---
+++ MoneyShare/MoneyShare/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MoneyShare.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
