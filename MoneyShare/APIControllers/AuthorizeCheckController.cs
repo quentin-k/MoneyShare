966dff2f4ba86a72c7935388d98540c26eae714e
Quentin <49767738+quentin-k@users.noreply.github.com>
2020-06-18 16:28:45 -0500

    Added files for welcoming user

---
+++ MoneyShare/APIControllers/AuthorizeCheckController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MoneyShare.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthorizeCheckController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        [Authorize]
+        public ActionResult Get()
+        {
+            return new OkResult();
+        }
+    }
+}
