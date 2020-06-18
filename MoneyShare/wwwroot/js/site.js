98553b166cc547ce63bbce9785b7e326e1dd27e7
Quentin <49767738+quentin-k@users.noreply.github.com>
2020-06-18 16:05:07 -0500

    commit message here

--- MoneyShare/wwwroot/js/site.js
+++ MoneyShare/wwwroot/js/site.js
@@ -144,38 +144,3 @@ document.getElementById("confirmTwoFactorButton").addEventListener("click", e =>
             enableLoginSpinner(false);
         });
 });
-function enableRegisterSpinner(on) {
-    var registerSpinner = document.getElementById('registerSpinner');
-    if (on)
-        registerSpinner.classList.remove('d-none');
-    else
-        registerSpinner.classList.add('d-none');
-}
-
-document.getElementById("forgotPasswordButton").addEventListener("click", e => {
-    var registerStatus = document.getElementById('registerStatus');
-    registerStatus.innerText = "";
-    enableRegisterSpinner(true);
-    var firstName = document.getElementById("firstName").value.trim();
-    var lastName = document.getElementById("lastName").value.trim();
-    var email = document.getElementById("email").value.trim();
-    var username = document.getElementById("username").value.trim();
-
-    data = { 'firstname': firstName, 'lastname': lastName, 'email': email, 'username': username };
-    myFetch('/api/ForgotPassword', 'POST', false, data)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-        })
-        .then(data => {
-            alert('Please check your email for a link to reset password.');
-            document.location.href = "/";
-            enableRegisterSpinner(false);
-        })
-        .catch(error => {
-            alert('Please check your email for a link to reset password.');
-            document.location.href = "/";
-            enableRegisterSpinner(false);
-        });
-});
