4eed940205e4820162b375e072564ee030f425d5
Quentin <49767738+quentin-k@users.noreply.github.com>
2020-06-27 00:21:48 -0500

    Fixed reset password

--- MoneyShare/wwwroot/js/resetPassword.js
+++ MoneyShare/wwwroot/js/resetPassword.js
@@ -1,10 +1,12 @@
 document.getElementById('ResetPasswordSubmit').addEventListener("click", e => {
-    var userId = document.getElementById('userId').value;
-    var token = document.getElementById('token').value;
+    var urlParams = new URLSearchParams(window.location.search);
+    var userId = urlParams.get('UserId');
+    var token = urlParams.get('Token');
     var password = document.getElementById('Password').value;
     var confirmPassword = document.getElementById('ConfirmPassword').value;
     if (password == confirmPassword) {
-        var data = { 'userId': userId, 'token': token, 'password': password, 'confirmPassword': confirmPassword };
+        var data = { 'UserId': userId, 'Token': token, 'Password': password, 'ConfirmPassword': confirmPassword };
+        console.log(data);
         var requestInfo = { 'method': 'POST', body: JSON.stringify(data), headers: { 'Content-Type': 'application/json' }, credentials: 'same-origin' };
         fetch('/api/ResetPassword', requestInfo)
             .then(response => {
